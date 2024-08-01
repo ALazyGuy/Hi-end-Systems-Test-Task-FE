9436073aea68eeb166f59db85a4cb89ae92f231f
ALazyGuy <razorprogrammer@gmail.com>
2024-08-01 10:38:04 +0300

    feat: Implemented popup

--- src/app/actions/pages/home-page/home-page.component.ts
+++ src/app/actions/pages/home-page/home-page.component.ts
@@ -4,23 +4,39 @@ import { BehaviorSubject, map, tap } from 'rxjs';
 import { UserInfo } from '../../../core/models/user-info';
 import { Router } from '@angular/router';
 import { AsyncPipe, NgIf } from '@angular/common';
+import { PopupComponent } from "../../components/popup/popup.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [NgIf, AsyncPipe],
+  imports: [NgIf, AsyncPipe, PopupComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent implements OnInit{
 
   userInfo$!: BehaviorSubject<UserInfo | null>;
+  isPopupOpen: boolean = false;
+  popupLabel: string = '';
 
   constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit(): void {
     this.userService.loadUserInfo().subscribe();
     this.userInfo$ = this.userService.getUserInfo$();
+  }
+
+  onControlClick(label: string) {
+    this.isPopupOpen = true;
+    this.popupLabel = label;
+  }
+
+  onPopupCancel(): void {
+    this.isPopupOpen = false;
+  }
+
+  onPopupSubmit(event: any) {
+    console.log(event);
   }
 
 }
